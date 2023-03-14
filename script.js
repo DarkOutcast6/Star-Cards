83ff92a783c8fb1ffe5ac358f9afd47cba323ce0
RedCoder <14816156-RedCoder@users.noreply.replit.com>
2023-03-14 00:21:55 +0000

    Mobile start

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // NOTE: DON'T USE THIS FOR ACTUAL SERVERS WITH BACKEND!
 // HAVE ACTUAL PATHS!
 
+if(/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+	location.replace("mobile.html");
+  document.write("mobile");
+}
+
 function hideAlert() {
 	document.elm.style.visibility = "hidden";
 }
+
