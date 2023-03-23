85b4e465306239326d96486164415f96ab17119c
RedCoder <14816156-RedCoder@users.noreply.replit.com>
2023-03-23 00:16:38 +0000

    Mobile!

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // NOTE: DON'T USE THIS FOR ACTUAL SERVERS WITH BACKEND!
 // HAVE ACTUAL PATHS!
 
-if(/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-	location.replace("mobile.html");
-  // document.write("mobile");
-}
+// if(/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+// 	location.replace("mobile.html");
+//   // document.write("mobile");
+// }
+
+// function navbar()
+// {
+// var navbar = "<nav>your navbar code goes here</nav>";
+// $('#navbar').html(navbar);
+// }
 
 function hideAlert() {
 	document.elm.style.visibility = "hidden";
